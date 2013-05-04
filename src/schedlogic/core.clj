2a86e57c5ca95bb422091c917042b5243d84b789
Emanuel Evans <emanuel.evans@gmail.com>
2013-05-03 19:51:11 -0700

    First working prototype!!!

--- src/schedlogic/core.clj
+++ src/schedlogic/core.clj
@@ -1,6 +1,53 @@
-(ns schedlogic.core)
+(ns schedlogic.core
+  (:refer-clojure :exclude [== distinct])
+  (:require [clojure.core.logic :refer [conde
+                                        lvars
+                                        run
+                                        fresh
+                                        everyg
+                                        ==]]
+            [clojure.core.logic.fd :refer [eq in interval distinct]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def day-intervals 24)
+
+(defn- zip [xs ys]
+  (map vector xs ys))
+
+(defn distinct-intervals [[[lb1 ub1] [lb2 ub2]]]
+  (conde
+   [(eq (<= ub1 lb2))]
+   [(eq (<= ub2 lb1))]))
+
+(defn constrain-task [[[start end] [earliest latest length]]]
+  (eq
+   (= (- end start) length)
+   (>= start earliest)
+   (<= end latest)))
+
+(defn task-domain [[start end]]
+  (in start end (interval day-intervals)))
+
+(defn schedule [n tasks appts]
+  "Find the first n ways to schedule tasks given set appts. Tasks should be a vector of [earliest latest length] vectors, and appts should be a vector of [start end] vectors."
+  (let [task-times (zip (lvars (count tasks)) (lvars (count tasks)))]
+    (run n [q]
+      (== q task-times)
+      (everyg task-domain task-times)
+      (everyg constrain-task (zip task-times tasks))
+      (let [tasks-and-appts (concat task-times appts)]
+        (everyg distinct-intervals (for [t1 tasks-and-appts
+                                         t2 tasks-and-appts
+                                         :when (not (= t1 t2))]
+                                     [t1 t2]))))))
+
+(comment
+
+  (schedule 2
+            [[2 6 2]
+             [2 6 2]
+             [1 8 1]
+             [7 20 7]]
+            [[7 9]
+             [6 7]])
+
+  )
