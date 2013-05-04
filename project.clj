f6460e1558d2cd6d7cb56e4047540fc2e8e9354f
Emanuel Evans <emanuel.evans@gmail.com>
2013-05-03 21:49:27 -0700

    Dependencies for web

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.3"]])
+                 [org.clojure/core.logic "0.8.3"]
+                 [cheshire "5.1.1"]
+                 [compojure "1.1.5"]
+                 [ring "1.1.8"]])
